0835026b11e15be97afb2d32285c3a4b9686bf7a
matti909 <matias.saantiago@gmail.com>
2023-11-21 12:14:32 -0300

    2

--- src/app/pages/cars/cars.module.ts
+++ src/app/pages/cars/cars.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CarsRoutingModule } from './cars-routing.module';
-import { CarComponent } from './car/car.component';
 import { MaterialModule } from 'src/app/material.module';
 import { CarsFilterComponent } from './cars-filter/cars-filter.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -12,12 +11,11 @@ import { PanelModule } from 'primeng/panel';
 import { TableModule } from 'primeng/table';
 import { CarsListComponent } from './cars-list/cars-list/cars-list.component';
 import { HomeComponent } from './home/home.component';
-import { PaginationComponent } from 'src/app/shared/components/pagination/pagination.component';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 @NgModule({
-    declarations: [CarComponent, CarsFilterComponent, CarsListComponent, HomeComponent, PaginationComponent],
+    declarations: [CarsFilterComponent, CarsListComponent, HomeComponent],
     imports: [CommonModule, CarsRoutingModule, MaterialModule, ReactiveFormsModule, DataViewModule, ButtonModule, PanelModule, TableModule, MatProgressSpinnerModule]
 })
 export class CarsModule { }
